c95070470a4611e8e6fc3c129a2d7d6daa296baf
gauravdwivedi <gauravdwivedi@hotmail.com>
2020-08-30 23:55:33 +0530

    comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+//cardova component
 import { Insomnia } from '@ionic-native/insomnia/ngx';
 @NgModule({
   declarations: [AppComponent],
