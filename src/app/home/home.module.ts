d0507d1e7f89214157ba360882df7338a9f3200b
gauravdwivedi <gauravdwivedi@hotmail.com>
2020-08-30 21:30:32 +0530

    Rest Timer Android Application-Inic Framework

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,15 +5,29 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+// Import ng-circle-progress
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    // Specify ng-circle-progress as an import
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+      animation: false,
+      responsive: true,
+      renderOnClick: false
+    })
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule { }
